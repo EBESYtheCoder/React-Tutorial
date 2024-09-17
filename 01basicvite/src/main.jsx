ac6ec7bee9d39d19ccca8b4f00e94225c4ba57e7
Outlier <nzombeebenezer@gmail.com>
2024-09-18 00:54:52 +0100

    basics of vite

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,7 +1,39 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+/*
+const ReactElement = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank'
+  },
+  childen: 'click me to visit google'
+}
+
+
+const areactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'click to visit google'
+)
+*/
+
+
+
+const AnotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React app</h1>
+    </div>
+  )
+}
 
 createRoot(document.getElementById('root')).render(
-  <App />
+  AnotherElement
 )
