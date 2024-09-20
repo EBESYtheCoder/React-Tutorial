63c2f5e5545482064780a86dd3487394a1ccfb52
Outlier <nzombeebenezer@gmail.com>
2024-09-20 19:31:44 +0100

    tailwind basiccs

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
 import './App.css'
 import Card from './combonents/Card'
@@ -5,12 +6,22 @@ import Card from './combonents/Card'
 function App() {
   const [count, setCount] = useState(0)
 
+  let myobj = {
+    name: 'Ebenezer',
+    address: {
+      city: 'Algiers',
+      state: 'Alger',
+      country: 'Algeria'
+    }
+  }
+  let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   return (
     <>
       <h1 className='text-3xl bg-green-500 p-3 rounded-md'
       >Vite with Tailwind </h1>
       <Card username="Ebenezer" />
-      <Card username="Bryan" url='https://tailwindcss.com/_next/static/media/sarah-dayan.de9b3815.jpg' />
+      <Card username="Bryan" myArray={newArr} />
       <Card username="Thamas" post="Staff Engineer" />
 
     </>
