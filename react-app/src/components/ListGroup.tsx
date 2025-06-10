b72199b1782adaf76c2d08857e8020ebb02516c8
Outlier <nzombeebenezer@gmail.com>
2025-06-11 00:22:31 +0100

    conditional rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,13 +1,46 @@
-// import { Fragment } from "react";
-function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+import { useState } from "react";
+
+// {items: [], heading: string}
+
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup(props: Props) {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+
+  // items = [];
+  // const message = items.length === 0 ? <p>No item found</p> : null;
+
+  // const getMessage = () => {
+  //   return items.length === 0 ? <p>No item found</p> : null;
+  // };
 
   return (
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
