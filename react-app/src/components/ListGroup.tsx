103935a716f55a4b7bbf2045b00dc7108bbdfe1f
Outlier <nzombeebenezer@gmail.com>
2025-06-10 22:43:22 +0100

    frangments

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+// import { Fragment } from "react";
+function ListGroup() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
